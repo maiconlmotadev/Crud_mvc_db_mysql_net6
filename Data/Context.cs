a56eda306cf03bcd46f17c453b904561080e73ba
Maicon Lourenço da Mota <maiconlmotadev@hotmail.com>
2023-02-08 13:30:17 +0000

    Update: controller creation and scaffolding

--- Data/Context.cs
+++ Data/Context.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
+using Microsoft.EntityFrameworkCore;
+using Crud_mvc_db_mysql_net6.Models;
 
 namespace Crud_mvc_db_mysql_net6.Data
 {
     public class Context: DbContext
     {
-        public Context(DbContextOptions<Context> options) : base(options)
+        public Context(DbContextOptions<Context> options)   
+            : base(options)
         { }
 
-
+        public DbSet<User> User { get; set; }
     }
 }
