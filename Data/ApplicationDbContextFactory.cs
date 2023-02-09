38b7eedafc98dc366265a1b4d0982b92a2315742
Maicon Lourenço da Mota <maiconlmotadev@hotmail.com>
2023-02-09 16:40:45 +0000

    Update: crud success

--- Data/ApplicationDbContextFactory.cs
+++ Data/ApplicationDbContextFactory.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Crud_mvc_db_mysql_net6.Data
 {
@@ -8,10 +9,24 @@ namespace Crud_mvc_db_mysql_net6.Data
         public Context CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<Context>();
-            optionsBuilder.UseMySql("server=127.0.0.1:3306;Initial Catalog=Crud_mvc_db_mysql_net6; uid=mac_win; pwd=M123456*",
-            Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"));
+            optionsBuilder.UseMySql(
+            "server=127.0.0.1; uid=mac_win; pwd=M123456*; database=Crud_mvc_db_mysql_net6",
+            Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"),
+
+            mySqlOptions =>
+            {
+
+                mySqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 2,
+                maxRetryDelay: TimeSpan.FromSeconds(30),
+                errorNumbersToAdd: null);
+            }
+
+            );
 
             return new Context(optionsBuilder.Options);
         }
+
+      
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,24 +1,37 @@
 using Crud_mvc_db_mysql_net6.Data;
 using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(); 
 
 var app = builder.Build();
 
 
 builder.Services.AddDbContext<Context>
     (options => options.UseMySql(
-            "server=127.0.0.1:3306;Initial Catalog=Crud_mvc_db_mysql_net6; uid=mac_win; pwd=M123456*",
-            Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql")));
+            "server=127.0.0.1; uid=mac_win; pwd=M123456*; database=Crud_mvc_db_mysql_net6",
+            Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.32-mysql"),
+
+            mySqlOptions =>
+            {
+
+                mySqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 2,
+                maxRetryDelay: TimeSpan.FromSeconds(30),
+                errorNumbersToAdd: null);
+            }
+
+            ));
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this fo r production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
